cb3919ec413c7dab4ac72ac8aea799edf477f6f3
pratham2609 <pratham1055.be21@chitkara.edu.in>
2024-01-13 17:06:27 +0530

    certificate upload changes done

--- server/controllers/certificate.js
+++ server/controllers/certificate.js
@@ -1,36 +1,6 @@
-// const ejs = require('ejs');
-// const fs = require('fs');
-// var Api2Pdf = require('api2pdf');
-// require('dotenv').config();
-// var a2pClient = new Api2Pdf(process.env.API_KEY);
-// const excelSchema = require('../models/excelSchema');
-// const sendInvoiceMail = require('../config/nodemailer');
-
-// const certificate = async (req, res) => {
-//     try {
-//         const renderdata = await excelSchema.find();
-//         const template = fs.readFileSync('views/certificate.ejs', 'utf-8');
-//         let data = renderdata[0]
-//         // Render the template with the data
-//         const renderedCertificate = ejs.render(template, { data });
-//         fs.writeFileSync('./cert.html', renderedCertificate);
-//         res.render('certificate', { data });
-//         const templatePrint = fs.readFileSync("./cert.html", "utf-8");
-//         a2pClient.wkHtmlToPdf(templatePrint).then(function (result) {
-//             sendInvoiceMail(data.Email, result.FileUrl)
-//         });
-//     }
-//     catch (error) {
-//         console.log(error)
-//     }
-// }
-
-
-// module.exports = { certificate }
-
-
 const xlsx = require('xlsx');
 const excelSchema = require('../models/excelSchema');
+const ExcelData = require('../models/excelSchema');
 const createCertificate = async (req, res) => {
     if (!req.file) {
         return res.status(400).json({ message: 'No file uploaded.' });
@@ -39,21 +9,37 @@ const createCertificate = async (req, res) => {
     const sheetName = workbook.SheetNames[0];
     const sheet = workbook.Sheets[sheetName];
     const jsonData = xlsx.utils.sheet_to_json(sheet);
+    var arrayToInsert = [];
+    for (var i = 0; i < jsonData.length; i++) {
+
+        // donot delete this
+        // const existingdata = await excelSchema.find({
+        //     eventsName: jsonData[i]["Event"],
+        //     // studentEmail: jsonData[i]["Email"],
+        //     // createdBy: req.user
+        // })
+        // if (!existingdata) {
+        //     var oneRow = {
+        //         studentName: jsonData[i]["Name"],
+        //         studentMobile: jsonData[i]["Mobile"],
+        //         studentRoll: jsonData[i]["Roll No"],
+        //         eventsName: jsonData[i]["Event"],
+        //         studentEmail: jsonData[i]["Email"]
+        //     }
+        //     arrayToInsert.push(oneRow);
+        // }
+        var oneRow = {
+            studentName: jsonData[i]["Name"],
+            studentMobile: jsonData[i]["Mobile"],
+            studentRoll: jsonData[i]["Roll No"],
+            eventsName: jsonData[i]["Event"],
+            studentEmail: jsonData[i]["Email"],
+            createdBy: req.user
+        }
+        arrayToInsert.push(oneRow);
+    }
     try {
-        jsonData.map(async (data) => {
-            const newData = {
-                ...data,
-                createdBy: req.user
-            }
-            const existingdata = await excelSchema.find({
-                rollNo: newData.rollNo,
-                createdBy: req.user
-            })
-            if (!existingdata && !existingdata.length > 0) {
-                const newExcelData = new excelSchema(newData);
-                await newExcelData.save();
-            }
-        })
+        ExcelData.insertMany(arrayToInsert);
         return res.status(200).json({ message: 'Data saved successfully.' });
     } catch (error) {
         res.status(500).json({ message: 'Error saving data to Database.' });
